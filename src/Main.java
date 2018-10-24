6a0c9a48afd44eb3ed177eda2ffa6ae086685dfd
antonyakovlev <anton2yakovlev@gmail.com>
2018-10-24 12:05:20 +0700

    del main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String s = "())(";
-        //Qwe a = new Qwe();
-        System.out.println(Qwe.correct(s, 0));
+
 
     }
 
