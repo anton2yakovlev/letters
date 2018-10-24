b80a897b7a6014f5e4878d0f429a299c0a9bf407
antonyakovlev <anton2yakovlev@gmail.com>
2018-10-24 17:10:06 +0700

    комплексные числа

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-
+        Les3 k = new Les3(3,5);
+        System.out.println(k.toString());
 
     }
 
+    public static class Les3 {
+        int a;
+        int b;
+        Les3 (int a, int b){
+            this.a = a;
+            this.b = b;
+        }
 
+        public String toString(){
+            return Integer.toString(a)+" + i*"+Integer.toString(b);
+        }
+
+
+    }
 
 }
