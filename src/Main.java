bf9a87f18f0ac94eedf6c11b926231b9f9931fcc
antonyakovlev <anton2yakovlev@gmail.com>
2018-10-24 14:36:31 +0700

    поиск максимума в массиве

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Les1.ex1(5, 3,2));
+        int [] a = {3, 2, 5, 6, 2, 1, 3};
+        System.out.println(Les1.ex2(a));
 
     }
 
--- src/lessons/Les1.java
+++ src/lessons/Les1.java
@@ -7,4 +7,21 @@ public class Les1 {
         }
         return b + ex1(a0, n-1,b);
     }
+
+    public static int ex2(int[] a) {
+        if (a.length>0) {
+            int max = a[0];
+            for (int i: a) {
+                if (i>max) {
+                    max = i;
+                }
+            }
+            return max;
+        } else {
+            return 0;
+        }
+    }
+
+
+
 }
