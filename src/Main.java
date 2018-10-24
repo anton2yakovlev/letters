72ca261072dfe3e5dff0633bf7aa51e8ea152bd4
antonyakovlev <anton2yakovlev@gmail.com>
2018-10-24 15:47:38 +0700

    сумма чисел кратных 3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,25 @@ public class Main {
     public static void main(String[] args) {
 
 
+    System.out.println(Les2.ex1(10));
+
+
     }
 
+    public static class Les2{
+
+
+        public static int ex1(int n){
+            int sum = 0;
+            for (int i = 0; i<=n; i++) {
+                if (i%3==0) {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+    }
 
 
 }
